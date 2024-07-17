1172b75fbaec674ffedb1bbf1b25ae8ba3e8593a
Deloz09 <84214640+TheDeloz-v2@users.noreply.github.com>
2024-07-16 18:05:23 -0600

    Revert "Modifiying with 3 different sets of data"
    This reverts commit eac90a8a69ca87da61e6cf0cac038e4f2232a7e8.

--- particleSwarmOp.py
+++ particleSwarmOp.py
@@ -8,7 +8,6 @@ to find the minimum of a function.
 
 import random as r
 import matplotlib.pyplot as plt
-import numpy as np
 
 '''
 Define the function to be minimized.
@@ -24,7 +23,7 @@ def f(x, y):
     return (x-3)**2 + (y-2)**2
 
 '''
-Initialize the list of particles
+initialize the list of particles
 
 Parameters:
     n (int): number of particles
@@ -48,48 +47,53 @@ def initialize_particles(n, x_min, x_max, v_min, v_max):
         
     return particles
 
-'''
-Function to plot particles
-'''
-def plot(particles, iteration):
+def plot(particles):
     x = [p["x"] for p in particles]
     y = [p["y"] for p in particles]
     plt.scatter(x, y)
     plt.xlim(-10, 10)
     plt.ylim(-10, 10)
     plt.grid(True)
-    plt.title(f"Iteration {iteration}")
-    plt.xlabel('x')
-    plt.ylabel('y')
     plt.show()
 
 '''
-Function to run the PSO algorithm
-
-Parameters:
-    w (float): inertia weight
-    c1 (float): cognitive coefficient
-    c2 (float): social coefficient
-    n (int): number of particles (default is 40)
-    iterations (int): number of iterations (default is 100)
-    x_min (float): minimum value of x (default is -10)
-    x_max (float): maximum value of x (default is 10)
-    v_min (float): minimum value of the velocity (default is -1)
-    v_max (float): maximum value of the velocity (default is 1)
-    
-Returns:
-    tuple: A tuple containing the best particle (dict) and the number of iterations (int)
-        - best particle (dict): The global best particle found with keys 'x', 'y', 'vx', 'vy', 'best_x', 'best_y', 'best_f'
-        - iterations (int): The number of iterations taken to find the best particle
+Main function
 '''
-def run_pso(w, c1, c2, n=40, iterations=100, x_min=-10, x_max=10, v_min=-1, v_max=1):
+def main():
+    # Number of particles
+    n = 40
+
+    # Search space
+    x_min = -10
+    x_max = 10
+
+    # Velocity
+    v_min = -1
+    v_max = 1
+
+    # Inertia weight
+    w = 0.5
+
+    # Hiperparameters
+    c1 = 1.5
+    c2 = 1.5
+
+    # Number of iterations
+    iterations = 100
+    
+    # Initialize particles
     particles = initialize_particles(n, x_min, x_max, v_min, v_max)
+
+    plot(particles)
+    
+    # Global best particle
     global_best = particles[0]
     for i in range(1, n):
         if particles[i]["best_f"] < global_best["best_f"]:
             global_best = particles[i]
-
+        
     for i in range(iterations):
+        
         r.seed(777)
         
         for j in range(n):
@@ -116,52 +120,11 @@ def run_pso(w, c1, c2, n=40, iterations=100, x_min=-10, x_max=10, v_min=-1, v_ma
             
         print("Iteration", i, ":", global_best["best_f"])
         
-        if i == iterations // 2 or i == iterations - 1 or i == iterations // 4:
-            plot(particles, i)
+        if i == iterations/2 or i == iterations-1 or i == iterations/4:
+            plot(particles)
         
-    return global_best, i
-
-'''
-Main function
-'''
-def main():
-    # Sets of parameters to test
-    parameter_sets = [
-        {'w': 0.3, 'c1': 1.3, 'c2': 1.3},
-        {'w': 0.7, 'c1': 1.7, 'c2': 1.7},
-        {'w': 0.9, 'c1': 2.0, 'c2': 2.0}
-    ]
-
-    results = []
-
-    for params in parameter_sets:
-        print(f"Testing with parameters: w={params['w']}, c1={params['c1']}, c2={params['c2']}")
-        result, iterations = run_pso(params['w'], params['c1'], params['c2'])
-        results.append((params, result, iterations))
-
-    # Print results
-    for params, result, iterations in results:
-        print(f"Parameters: w={params['w']}, c1={params['c1']}, c2={params['c2']}")
-        print(f"Global best particle: {result}")
-        print(f"Took {iterations} iterations")
-
-    # Contour plot of the function
-    x = np.linspace(-10, 10, 400)
-    y = np.linspace(-10, 10, 400)
-    X, Y = np.meshgrid(x, y)
-    Z = f(X, Y)
-
-    plt.contour(X, Y, Z, levels=50, cmap='jet')
-    plt.title('Contour plot of the function')
-
-    # Plot the global best positions found
-    for params, result, _ in results:
-        plt.plot(result['x'], result['y'], 'o', label=f"w={params['w']}, c1={params['c1']}, c2={params['c2']}")
-
-    plt.legend()
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.show()
+    print("Global best particle:", global_best)
+        
 
 if __name__ == '__main__':
     main()
